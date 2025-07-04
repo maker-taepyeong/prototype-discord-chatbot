f4d78b32b4052d947176385d1337d81a3a66318f
taepyeong <taepyeong.dev@gmail.com>
2025-07-04 20:28:45 +0900

    chore: discord 기본 기능 추가

---
+++ src/prototype_discord_chatbot/main.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True  # 핵심
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"{client.user} is ready.")
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return  # 자기 자신/다른 봇은 무시
+
+    if client.user in message.mentions:
+        # GPT에 유저 메시지 전달
+        prompt = message.content.replace(f"<@{client.user.id}>", "").strip()
+
+        await message.channel.send("test")
+
+client.run(os.getenv("DISCORD_BOT_TOKEN"))
